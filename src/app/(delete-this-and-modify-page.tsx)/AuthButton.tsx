9b262ec5b842c1da1aa785f152eed40419187c19
Siddhartha Maity <siddharthamaity1993@gmail.com>
2024-11-24 14:42:41 +0530

    Add example environment variables and handle optional user image and name in AuthButton

--- src/app/(delete-this-and-modify-page.tsx)/AuthButton.tsx
+++ src/app/(delete-this-and-modify-page.tsx)/AuthButton.tsx
@@ -7,7 +7,7 @@ const AuthButton = () => {
 
     return session?.user ? (
         <div className='flex items-center gap-3'>
-            <img className='size-9 rounded-full' src={session.user.image} alt={session.user.name} />
+            <img className='size-9 rounded-full' src={session.user.image ?? ''} alt={session.user.name ?? ''} />
             <div className=''>Hi, {session.user.name}</div>
             <button className='rounded-full bg-red-500 bg-opacity-10 px-3 py-2 text-red-500' onClick={() => signOut()}>
                 Logout
