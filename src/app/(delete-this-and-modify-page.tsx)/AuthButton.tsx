7b11f9490a524179259c91d6e77682d44af39544
Siddhartha Maity <siddharthamaity1993@gmail.com>
2024-11-24 14:34:03 +0530

    Initial commit

---
+++ src/app/(delete-this-and-modify-page.tsx)/AuthButton.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { signIn, signOut, useSession } from 'next-auth/react';
+
+const AuthButton = () => {
+    const { data: session } = useSession();
+
+    return session?.user ? (
+        <div className='flex items-center gap-3'>
+            <img className='size-9 rounded-full' src={session.user.image} alt={session.user.name} />
+            <div className=''>Hi, {session.user.name}</div>
+            <button className='rounded-full bg-red-500 bg-opacity-10 px-3 py-2 text-red-500' onClick={() => signOut()}>
+                Logout
+            </button>
+        </div>
+    ) : (
+        <button
+            className='rounded-full bg-blue-500 bg-opacity-10 px-3 py-2 text-blue-500'
+            onClick={() => signIn('github')}>
+            Login
+        </button>
+    );
+};
+
+export default AuthButton;
